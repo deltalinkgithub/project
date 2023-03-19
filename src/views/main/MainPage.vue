926025b0948adcde9d24d89e129abf43300cd0cb
Jin <deltalink025@gmail.com>
2023-03-19 14:37:52 +0000

    add main page

---
+++ src/views/main/MainPage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="main">
+        <div id="search-page">
+            <el-card class="box-card">
+                <span id="title">JIN'S 購物天堂</span>
+            </el-card>
+        </div>
+    </div>
+</template>
+<style src="@/assets/css/main/main.css"></style>
