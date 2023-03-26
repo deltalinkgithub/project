df3f694f8566076df97750a69c48f3febe867bb4
Jin <deltalink025@gmail.com>
2023-03-26 15:08:54 +0000

    add_cart_component

--- src/views/main/MainPage.vue
+++ src/views/main/MainPage.vue
@@ -5,15 +5,18 @@
                 <span id="title">JIN'S 購物天堂</span>
                 <!-- <a href="https://www.flaticon.com/free-icons/login" title="login icons">icons created by srip - Flaticon</a> -->
                 <div id="icon-block">
-                    <span class="icon-user"></span>
-                    <span class="icon-cart"></span>
+                    <!-- <span class="icon-user"></span> -->
+                    <jin-cart id="cart"></jin-cart>
                 </div>
                 <div id="tag-block">
-                    <el-button type="text">首頁</el-button>
-                    <el-button type="text">關於我們</el-button>
+                    <jin-tag>首頁</jin-tag>
+                    <jin-tag>關於我們</jin-tag>
+                    <jin-tag>聯絡方式</jin-tag>
                 </div>
             </el-card>
         </div>
     </div>
 </template>
+<script>
+</script>
 <style src="@/assets/css/main/main.css"></style>
